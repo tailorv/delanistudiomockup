4c17a5b34ff64386d5f648d4558524ee7eb7a67b
tailorv <maxwellmuthomijr@gmail.com>
2021-06-20 12:08:33 +0300

    update feedback message

--- js/script.js
+++ js/script.js
@@ -77,7 +77,7 @@ $(document).ready(function() {
         $("#eight").hide();
     });
 
-    //submit message
+    //submit message reply / feedback 
     $("form#my-form").submit(function(event) {
         event.preventDefault();
         var name = $("#name").val();
